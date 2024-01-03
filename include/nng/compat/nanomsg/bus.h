2f9fa236acc8f4f56d656b2fd3cb809574239508
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2024-01-03 22:54:01 +0000

    nng 1.7.0 mbedtls 3.5.1

---
+++ include/nng/compat/nanomsg/bus.h
@@ -0,0 +1,26 @@
+//
+// Copyright 2018 Staysail Systems, Inc. <info@staysail.tech>
+// Copyright 2018 Capitar IT Group BV <info@capitar.com>
+//
+// This software is supplied under the terms of the MIT License, a
+// copy of which should be located in the distribution where this
+// file was obtained (LICENSE.txt).  A copy of the license may also be
+// found online at https://opensource.org/licenses/MIT.
+//
+
+#ifndef NNG_COMPAT_BUS_H
+#define NNG_COMPAT_BUS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define NN_PROTO_BUS 7
+#define NN_BUS (NN_PROTO_BUS * 16 + 0)
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // NNG_COMPAT_BUS_H
