8ecdddd0cb51ed82068b10c599f43f6923ebe5e1
shikokuchuo <53399081+shikokuchuo@users.noreply.github.com>
2024-04-28 14:23:12 +0100

    patch nng 1.7.3 -> 1.8.0

--- include/nng/nng.h
+++ include/nng/nng.h
@@ -44,8 +44,8 @@ extern "C" {
 #endif
 
 #define NNG_MAJOR_VERSION 1
-#define NNG_MINOR_VERSION 7
-#define NNG_PATCH_VERSION 3
+#define NNG_MINOR_VERSION 8
+#define NNG_PATCH_VERSION 0
 #define NNG_RELEASE_SUFFIX \
 	"" // if non-empty (i.e. "pre"), this is a pre-release
 
